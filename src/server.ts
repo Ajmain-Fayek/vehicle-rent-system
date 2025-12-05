c06d5fc8edd29c435a221b3080910ad6e205f710
Ajmain-Fayek <ajmainfayek733@gmail.com>
2025-12-05 17:28:45 +0600

    feat(server)/ initialize Express app with DB connection check

--- src/server.ts
+++ src/server.ts
@@ -6,15 +6,27 @@ const app = express();
 
 app.use(express.json());
 
+//==========================//
+//        Test APIs         //
+//==========================//
 app.get("/", async (req: Request, res: Response) => {
   res.send({ success: true, message: "Welcome to the Vehicle Rental System portal." });
 });
+
 app.get("/health-check", async (req: Request, res: Response) => {
   res.send({ success: true, message: "Health Ok." });
 });
 
-app.listen(envConfig.port, () => {
-  console.log(`Server is running at PORT: ${envConfig.port} - http://localhost:${envConfig.port}`);
-});
+// ---------------------//
+// Production API's     //
+// ---------------------//
 
-checkDatabaseConnection();
+
+// -----------------------------------------------------//
+// Start the server after DB connection is established  //
+// -----------------------------------------------------//
+checkDatabaseConnection().then(() => {
+  app.listen(envConfig.port, () => {
+    console.log(`Server is running at PORT: ${envConfig.port} - http://localhost:${envConfig.port}`);
+  });
+});
