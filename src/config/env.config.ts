470acb0a0fd73f1a8a5b22f597b8d9e727fb16b5
Ajmain-Fayek <ajmainfayek733@gmail.com>
2025-12-06 02:58:45 +0600

    chore(env)/ add salt round

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -7,6 +7,7 @@ const envConfig = {
   env: process.env.NODE_ENV,
   port: process.env.PORT ? Number(process.env.PORT) : 5000,
   pgDb: process.env.DB_URL, // PostgreSQL
+  saltRounds: process.env.SALT_ROUNDS ? Number(process.env.SALT_ROUNDS) : 10,
 };
 
 export default envConfig;
