e39a1ae221e7762b8935693926658ce4d56289a1
Ajmain-Fayek <ajmainfayek733@gmail.com>
2025-12-06 03:04:37 +0600

    feat(auth)/ add new user with password hashing

---
+++ src/controllers/authentication/signupController.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import { TApiResponse } from "../../types/apiResponse";
+import pool from "../../config/pgDb.config";
+import bcrypt from "bcrypt";
+import envConfig from "../../config/env.config";
+import { sendResponse } from "../../utils/sendResponse";
+
+export const signupController = async (req: Request, res: Response): Promise<any> => {
+  if (!req.validatedPayload) {
+    return res.status(400).json({
+      success: false,
+      message: "Payload missing",
+    } as TApiResponse);
+  }
+
+  const { name, email, password, phone, role } = req.validatedPayload!;
+
+  try {
+    const existingUser = await pool.query("SELECT id FROM users WHERE email = $1", [email]);
+
+    if (existingUser.rows.length !== 0) {
+      return sendResponse(res, 400, false, "User already exists.");
+    }
+
+    const hashedPassword = await bcrypt.hash(password as string, envConfig.saltRounds);
+
+    const response = await pool.query(
+      "INSERT INTO users (name, email, password, phone, role) VALUES ( $1, $2, $3, $4, $5 ) RETURNING id, name, email, phone, role",
+      [name, email, hashedPassword, phone, role]
+    );
+
+    return sendResponse(res, 201, true, "User registered successfully", response.rows[0]);
+  } catch (error: any) {
+    return sendResponse(res, 500, false, error.message);
+  }
+};
