baa4b73e3efa5af96191854f0053cd39f65c7387
Ajmain-Fayek <ajmainfayek733@gmail.com>
2025-12-07 00:29:59 +0600

    feat(env.config)/ add private and public key to env.config

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,13 +1,19 @@
 import dotenv from "dotenv";
 import path from "path";
+import fs from "fs";
 
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
+const privateSecret = fs.readFileSync(path.join(process.cwd(), "private.key"), "utf8");
+const publicSecret = fs.readFileSync(path.join(process.cwd(), "public.key"), "utf8");
+
 const envConfig = {
-  env: process.env.NODE_ENV,
+  env: String(process.env.NODE_ENV),
   port: process.env.PORT ? Number(process.env.PORT) : 5000,
-  pgDb: process.env.DB_URL, // PostgreSQL
+  pgDb: String(process.env.DB_URL), // PostgreSQL
   saltRounds: process.env.SALT_ROUNDS ? Number(process.env.SALT_ROUNDS) : 10,
+  jwtPrivateSecret: privateSecret,
+  jwtPublicSecret: publicSecret,
 };
 
 export default envConfig;
