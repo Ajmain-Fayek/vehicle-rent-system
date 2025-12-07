59a37e50c9007002bdb2a72fccfc83462336a252
Ajmain-Fayek <ajmainfayek733@gmail.com>
2025-12-07 14:52:52 +0600

    feat(middleware)/ add sanitization of incomming payload

---
+++ src/middlewares/validateUpdatingPayload.ts
@@ -0,0 +1,37 @@
+import { Response, Request, NextFunction } from "express";
+import { sendResponse } from "../utils/sendResponse";
+
+/**
+ * middleware to validate incomming requst body payload
+ * @param requiredFields Array of fields
+ * @returns injects sanitized data object into request header as sanitizedUpdatingPayload
+ */
+
+export const validateUpdatingPayload = (requiredFields: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const sanitizedFields: string[] = [];
+    const sanitizedValues: string[] = [];
+
+    for (const field of requiredFields) {
+      const value = req.body?.[field];
+      if (!value || !String(value).trim()) {
+        continue;
+      }
+
+      sanitizedFields.push(String(field));
+      sanitizedValues.push(String(value));
+    }
+
+    if (sanitizedFields.length === 0 || sanitizedValues.length === 0) {
+      return sendResponse(res, 400, false, ["Error sanitizing update fields", "No update information are provided"]);
+    }
+
+    req.sanitizedUpdatingPayload = requiredFields.reduce((acc, field) => {
+      acc[field] = req.body[field];
+
+      return acc;
+    }, {} as Record<string, string>);
+
+    next();
+  };
+};
